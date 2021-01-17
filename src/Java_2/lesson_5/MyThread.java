dcd71c8c489f1b24ce74ba10d573a32d1b1fd697
Mikhail1000 <43309156+Mikhail1000@users.noreply.github.com>
2021-01-17 18:21:46 +0500

    Работает.

---
+++ src/Java_2/lesson_5/MyThread.java
@@ -0,0 +1,16 @@
+package Java_2.lesson_5;
+
+public class MyThread implements Runnable{
+    float[] arr;
+
+    public MyThread(float[] arr) {
+        this.arr = arr;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+    }
+}
