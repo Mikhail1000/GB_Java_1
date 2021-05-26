66d90774890277a886a4bdf79347be584a401838
Mikhail1000 <43309156+Mikhail1000@users.noreply.github.com>
2021-05-26 21:03:10 +0500

    Сделал имена классов в единственном числе

--- src/Java_1/lesson_7/Main.java
+++ src/Java_1/lesson_7/Main.java
@@ -1,25 +1,25 @@
-package lesson_7;
+package Java_1.lesson_7;
 
 import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        Cats cat1 = new Cats("Барсик", 10);
-        Cats cat2 = new Cats("Мурзик", 12);
-        Cats cat3 = new Cats("Чернышь", 5);
-        Cats cat4 = new Cats("Пушистик", 7);
-        Plates plate = new Plates();
+        Cat cat1 = new Cat("Барсик", 10);
+        Cat cat2 = new Cat("Мурзик", 12);
+        Cat cat3 = new Cat("Черныш", 5);
+        Cat cat4 = new Cat("Пушистик", 7);
+        Plate plate = new Plate();
 
         final int numberDay = 3;
         Random random = new Random();
-        Cats catsArr[] = {cat1, cat2, cat3, cat4};
+        Cat catsArr[] = {cat1, cat2, cat3, cat4};
 
         for (int i = 0; i < numberDay; i++) {
             int numberFoodToday = random.nextInt(40);
             System.out.println("Настал новый день");
             plate.addFood(numberFoodToday);
-            for (Cats cat : catsArr) {
+            for (Cat cat : catsArr) {
                 cat.eat(plate);
             }
             System.out.println("День идет к концу");
